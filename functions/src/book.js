5c75754df86acf5004818ecaccc3acc006d4417a
Benoit Zohar <benoit.zohar@gmail.com>
2020-02-27 22:57:56 +0000

    try es5 in book.js

--- functions/src/book.js
+++ functions/src/book.js
@@ -2,11 +2,11 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./google-credentials.json");
 
-export default async function book() {
+exports.book = async function book() {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://ymca-booker.firebaseio.com"
   });
 
   return "Book OK.";
-}
+};
