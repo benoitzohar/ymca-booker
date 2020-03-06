451aa4014be826e509d41d336cd25d418fc40f26
Benoit Zohar <benoit.zohar@gmail.com>
2020-03-06 03:34:43 +0000

    log token

--- functions/src/api.js
+++ functions/src/api.js
@@ -8,6 +8,8 @@ exports.checkAuthorization = function checkAuthorization(event) {
   }
 
   const { token } = querystring.parse(event.body);
+  console.log("[BZ] API_TOKEN:", API_TOKEN);
+  console.log("[BZ] token:", token);
   if (!token || token !== API_TOKEN) {
     return { statusCode: 403, body: "Not Allowed" };
   }
